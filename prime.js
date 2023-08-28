114e3c4f9f81dd5ea287653e491205cdea649ee9
Sanjana Singh <sanjanas2201@gmal.com>
2023-08-28 12:35:09 +0530

    prime.js

---
+++ prime.js
@@ -0,0 +1,13 @@
+let count = 0;
+let num = 9;
+
+for (let i = 1; i <= num; i++) {
+  if (num % i) {
+    count++;
+  }
+}
+if (count > 2) {
+  console.log("Prime Number");
+} else {
+  console.log("Composite Number");
+}
